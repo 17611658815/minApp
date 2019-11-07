96f97e0b6bc482998e9f41436b403116f530de25
17611658815 <17611658815@163.com>
2019-11-07 10:21:40 +0800

    2019-11-07

---
+++ 在线课堂/siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "盯盯课堂",
+    uniacid: "7",
+    acid: "7",
+    multiid: "0",
+    version: "4.6.0",
+    siteroot: "https://a.looioo.com/app/index.php",
+    design_method: "3"
+};
